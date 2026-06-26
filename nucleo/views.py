59f29c39214938727fe136a3fb4d658d5951b621
oscarjvn <oscarjosevilchez2@gmail.com>
2026-06-26 10:42:51 -0400

    starting with the first view

--- nucleo/views.py
+++ nucleo/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse, JsonResponse, Http404
 # Create your views here.
+def home_page(request):
+    return HttpResponse("Granja pedidos")
